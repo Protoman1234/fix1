cd6758c4e8975a9a185be1a82d89d8b3ff8b9641
Protoman1234 <rffrgtdgd@gmail.com>
2024-07-25 11:54:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ if (cluster.isPrimary) {
 const app = require('express')()
 const authenticate = require('./src/authenticate')
 const params = require('./src/params')
-const proxy = require('./src/proxy')
+
 
 const PORT = process.env.PORT || 8080
 
-app.enable('trust proxy')
-app.get('/', authenticate, params, proxy)
+
+app.get('/', authenticate, params)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
